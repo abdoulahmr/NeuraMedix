5a97f11d2c71feccb498c2a90d83a3da0139684c
abdoulahmr <202034014503@univ-tebessa.dz>
2025-06-09 16:10:54 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,19 @@ function App() {
   }, []);
   return (
     <>
+      <head>
+        <title>NeuraMedix</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="NeuraMedix is an all-in-one AI-powered software platform designed to accelerate biomedical research." />
+        <meta name="author" content="Lahmar abdessalem" />
+        <meta name="keywords" content="AI, biomedical research, software platform, NeuraMedix, machine learning, data analysis, healthcare innovation" />
+        <meta property="og:title" content="NeuraMedix - Accelerating Biomedical Research with AI" />
+        <meta property="og:description" content="NeuraMedix is an all-in-one AI-powered software platform designed to accelerate biomedical research." />
+        <meta property="og:image" content="/img/og-image.png" />
+        <meta property="og:url" content="https://neura-medix.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/img/4.png" />
+      </head>
       <Header />
       <HeroSection />
       <FeaturesSection />
